3d6cb9cd39e202afa8762d041f6ee93d1fe1491c
user1 <user1@gmail.com>
2025-03-26 15:39:35 +0000

    Handle loading state in posts page provider

---
+++ src/components/atoms/Loading/Loading.tsx
@@ -0,0 +1,3 @@
+import { FC } from "react";
+
+export const Loading: FC = () => <div role="status">Loading...</div>;
--- src/components/organisms/PostsPage/PostsPageProvider.tsx
+++ src/components/organisms/PostsPage/PostsPageProvider.tsx
@@ -2,17 +2,26 @@ import { fetchAllPosts } from "@/services/posts";
 import { Post } from "@/types";
 import { FC, useEffect, useState } from "react";
 import { PostsPage } from "./PostsPage";
+import { Loading } from "@/components/atoms";
 
 export const PostsPageProvider: FC = () => {
+  const [loading, setLoading] = useState(false);
   const [posts, setPosts] = useState<Post[] | null>();
 
   useEffect(() => {
     const getPosts = async () => {
+      setLoading(true);
       const posts = await fetchAllPosts();
       setPosts(posts);
+      setLoading(false);
     };
     getPosts();
   }, []);
 
-  return <>{posts && <PostsPage posts={posts} />}</>;
+  return (
+    <>
+      {loading && <Loading />}
+      {posts && <PostsPage posts={posts} />}
+    </>
+  );
 };
