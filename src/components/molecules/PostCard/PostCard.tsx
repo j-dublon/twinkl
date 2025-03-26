9a5d02290719d6c3af0243eedadee0df80bef4e2
user1 <user1@gmail.com>
2025-03-26 16:19:51 +0000

    Add basic tailwind css to PostCard component

--- src/components/molecules/PostCard/PostCard.tsx
+++ src/components/molecules/PostCard/PostCard.tsx
@@ -2,9 +2,12 @@ import { PostCardProps } from "@/types";
 import { FC } from "react";
 
 export const PostCard: FC<PostCardProps> = ({ post }) => (
-  <div>
+  <div
+    className="m-4 pb-1 focus:outline-none focus:ring-2 focus:ring-blue-500 border-b-1 border-b-zinc-300"
+    tabIndex={0}
+    aria-label="Post content"
+  >
     <h1>{post.title}</h1>
     <p>{post.body}</p>
-    <br />
   </div>
 );
