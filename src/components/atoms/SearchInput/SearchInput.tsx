2cec5dca38ae90a139f7ce352522157005d5bd5e
user1 <user1@gmail.com>
2025-03-27 08:24:17 +0000

    Add responsive styling to SearchInput component

--- src/components/atoms/SearchInput/SearchInput.tsx
+++ src/components/atoms/SearchInput/SearchInput.tsx
@@ -3,13 +3,16 @@ import { SearchInputProps } from "@/types";
 
 export const SearchInput: FC<SearchInputProps> = ({ value, onChange }) => {
   return (
-    <input
-      placeholder="Search..."
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-        onChange(e.target.value)
-      }
-      value={value}
-      type="search"
-    />
+    <div className="bg-zinc-100 py-2 sm:bg-white">
+      <input
+        placeholder="Search..."
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          onChange(e.target.value)
+        }
+        value={value}
+        type="search"
+        className="block m-auto bg-white sm:bg-zinc-100 p-2 rounded-sm w-90/100 sm:w-40/100 lg:w-30/100"
+      />
+    </div>
   );
 };
