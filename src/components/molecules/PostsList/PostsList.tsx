7570e7b5d4de2b9160b130e1b19abf95438600bb
user1 <user1@gmail.com>
2025-03-27 08:26:39 +0000

    Add responsive design to PostsList component

--- src/components/molecules/PostsList/PostsList.tsx
+++ src/components/molecules/PostsList/PostsList.tsx
@@ -3,9 +3,9 @@ import { FC } from "react";
 import { PostCard } from "../PostCard/PostCard";
 
 export const PostsList: FC<PostListProps> = ({ posts, removePost }) => (
-  <>
+  <div className="sm:w-70/100 lg:w-60/100 m-auto">
     {posts.map((post) => (
       <PostCard post={post} removePost={removePost} key={post.id} />
     ))}
-  </>
+  </div>
 );
